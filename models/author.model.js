ac4b57e93ab59483da05526bebf2c4678fd14c82
Javier Andrade <82067853+Javier1600@users.noreply.github.com>
2022-08-19 11:40:03 -0500

    modified:   models/author.model.js

--- models/author.model.js
+++ models/author.model.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const AuthorScheme = new mongoose.Schema({
     authorName: {
         type:  String,
-        required: [true, 'Authors name is mandatory']
+        required: [true, 'Authors name is mandatory'],
+        minlength: [3, "Authors name must be at least 3 chars"]
     }
 });
 
